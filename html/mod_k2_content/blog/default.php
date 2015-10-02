a1ef1d7129db7022ec3d7e8008b005494576b7e8
Kelsey Brookes <Kelsey@Prometheus.local>
2015-10-03 09:32:48 +1000

    Update blog module and component to handle greyscale hover functions

--- html/mod_k2_content/blog/default.php
+++ html/mod_k2_content/blog/default.php
@@ -20,10 +20,15 @@ defined('_JEXEC') or die ;
     <?php foreach ($items as $key=>$item):	?>
 	    <div class="col-xs-12 col-sm-6 blogPostItem" data-mh="blogPostItem">
 		    <a title="<?php echo JText::_('K2_CONTINUE_READING'); ?> &quot;<?php echo htmlspecialchars($item->title); ?>&quot;"  href="<?php echo $item->link; ?>" class="blogPostButton">
-			    <div class="blogPostButton">
-			    <?php if($params->get('itemImage') && $item->image): ?>
-			    	<img src="<?php echo $item->image->src; ?>" alt="<?php echo htmlspecialchars($item->image->alt); ?>" class="img-responsive"/>
-		    	<?php endif; ?>
+			    <div class="blogPostButton" >
+				    <?php if($params->get('itemImage')  && ($item->image)): ?>
+				    	<img src="<?php echo $item->image->src; ?>" class="blogItemImage" />
+			    	<?php endif; ?>
+			    	<div class="blogItemReadMore text-center">
+						<button class="btn btn-default btn-lg btn-green" >
+							Read more <i class="fa fa-angle-double-right"></i>
+						</button>
+					</div>
 			    </div>
 		    </a>
 		    <h3><?php echo $item->title; ?></h3>
@@ -48,4 +53,3 @@ defined('_JEXEC') or die ;
 		</div>
 	</div>
 </div>
-
