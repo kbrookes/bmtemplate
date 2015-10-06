2a3a10da86145a2ec2f291d5260daa6df611673d
Kelsey Brookes <Kelsey@Prometheus.local>
2015-10-06 12:23:35 +1000

    general updates

--- html/mod_k2_content/case-studies/default.php
+++ html/mod_k2_content/case-studies/default.php
@@ -25,17 +25,17 @@ if ($menu->getActive() == $menu->getDefault()) {
 <?php if(count($items)): ?>
 	<div class="row no-gutters">
     <?php foreach ($items as $key=>$item):	?>
-	    <div class="col-xs-12 col-sm-4 caseStudyItem" style="background-image: url(<?php echo $item->image->src; ?>);">
+	    <div class="col-xs-12 col-sm-4 caseStudyItem" <?php if($params->get('itemImage') && $item->image): ?>style="background-image: url(<?php echo $item->image->src; ?>);"<?php endif; ?>>
 		    <?php if(($isFrontPage) && ($ualayout != "mobile")):?>
-		    <a title="<?php echo JText::_('K2_CONTINUE_READING'); ?> &quot;<?php echo htmlspecialchars($item->title); ?>&quot;" type="button" data-toggle="collapse" data-target="#caseStudyContent-<?php echo $key;?>" aria-expanded="false" aria-controls="caseStudyContent" data-parent="#caseStudiesHome" class="caseStudyButton collapsed button<?php echo $key; ?>">
+		    <a title="<?php echo JText::_('K2_CONTINUE_READING'); ?> &quot;<?php echo htmlspecialchars($item->title); ?>&quot;" type="button" data-toggle="collapse" data-target="#caseStudyContent-<?php echo $key;?>" aria-expanded="false" aria-controls="caseStudyContent" data-parent="#caseStudiesHome" class="caseStudyButton collapsed">
 		    <?php else: ?>
-		    <a href="<?php echo $item->link; ?>" class="caseStudyButton collapsed">
+		    <a href="<?php echo $item->link; ?>">
 		    <?php endif; ?>
-			    <div class="caseStudyButtonInner">
-			    <?php if($item->extraFields->casestudiesclientlogo->value != ''): 
-				    echo $item->extraFields->casestudiesclientlogo->value;
-			    endif; ?>
-			    </div>
+			    <div class="caseStudyButton">
+				    <div class="caseStudyButtonInner">
+				    <?php echo $item->extraFields->casestudiesclientlogo->value; ?>
+				    </div>
+				</div>
 		    </a>
 	    </div>
     <?php endforeach; ?>
@@ -45,25 +45,32 @@ if ($menu->getActive() == $menu->getDefault()) {
 	<?php if(($ualayout == "desktop") || ($ualayout == "tablet")){?>
 		<?php if(count($items)): ?>
 		    <?php foreach ($items as $key=>$item):	?>
-		    <div class="caseStudyContent collapse" id="caseStudyContent-<?php echo $key;?>" <?php if(isset($item->extraFields->teaserheroimage->value)):?>style="background-image: url(<?php echo $item->extraFields->teaserheroimage->value; ?>)"<?php endif; ?>>
-			    <a class="closeCaseStudy"><i class="icon-close"></i></a>
+		    <div class="caseStudyContent collapse" id="caseStudyContent-<?php echo $key;?>">
 			    <div class="row">
-				    <div class="col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-5 col-lg-offset-1 caseStudyContentCol">
-					    <?php if($params->get('itemIntroText')): ?>
-							<?php echo $item->introtext; ?>
-						<?php endif; ?>
-						<?php if(($params->get('itemReadMore') && $item->fulltext) || ($params->get('itemCategory'))): ?>
-							<div class="bannerButtons verticalButtons">
-								<?php if($params->get('itemReadMore') && $item->fulltext): ?>
-									<a href="<?php echo $item->link; ?>" class="btn btn-default btn-lg btn-dark matchWidth">Read more <i class="fa fa-angle-double-right"></i></a>
-								<?php endif; ?>
-								<?php if($params->get('itemCategory')): ?>
-									<a class="btn btn-default btn-lg btn-dark matchWidth" href="/work">More <?php echo $item->category->title; ?> <i class="fa fa-angle-double-right"></i></a>
-								<?php endif; ?>
-							</div>
-						<?php endif; ?>
-				    </div>
 				    
+				    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 caseStudyContentCol">
+					    <div class="caseStudyContentColInner">
+						    <?php if($params->get('itemIntroText')): ?>
+								<?php echo $item->introtext; ?>
+							<?php endif; ?>
+							<?php if(($params->get('itemReadMore') && $item->fulltext) || ($params->get('itemCategory'))): ?>
+								<div class="bannerButtons verticalButtons">
+									<?php if($params->get('itemReadMore') && $item->fulltext): ?>
+										<a href="<?php echo $item->link; ?>" class="btn btn-default btn-lg btn-dark matchWidth">Read more <i class="fa fa-angle-double-right"></i></a>
+									<?php endif; ?>
+									<?php if($params->get('itemCategory')): ?>
+										<a class="btn btn-default btn-lg btn-dark matchWidth" href="/work">More <?php echo $item->category->title; ?> <i class="fa fa-angle-double-right"></i></a>
+									<?php endif; ?>
+								</div>
+							<?php endif; ?>
+					    </div>
+				    </div>
+				    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6  caseStudyImage">
+					    <?php if(isset($item->extraFields->teaserheroimage->value)): ?>
+						    <img src="<?php echo $item->extraFields->teaserheroimage->value; ?>" class="img-responsive" />
+						<?php endif; ?>
+						<a class="closeCaseStudy"><i class="icon-close"></i></a>
+				    </div>
 			    </div>
 		    </div>
 		    <?php endforeach; ?>
